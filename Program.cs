a3802c6cedaf7720c9d2f4cb6f2c997ebad013ce
TEJAS <tejasgowda0524@gmail.com>
2024-03-04 23:40:02 +0530

    ..

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿namespace generic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
+            char[] ch = { 'a', 'b', 'c', 'd' };
+            string[] strArr = { "one", "two", "three" };
+
+           
+            demo2(arr);
+            //demo2(strArr);
+            Console.ReadLine();
+        }
+        /* static void demo(int[] ele)
+     {
+         foreach (int a in ele)
+         {
+             Console.WriteLine(a);
+         }
+     }*/
+
+        //static void demo1(char[] chArr)
+        //{
+        //    foreach (char val in chArr)
+        //    {
+        //        Console.WriteLine(val);
+        //    }
+
+        //}
+
+
+        public static void demo2<T>(T [] val)
+        {
+            
+            foreach (T ele in val)
+            {
+                Console.WriteLine(ele);
+            }
+        }
+
+    }
+}
+
+
